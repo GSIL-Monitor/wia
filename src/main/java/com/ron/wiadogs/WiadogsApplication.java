561a9d00af9958c5c6177d550e0219fbded76bab
ron <rao261488647@126.com>
2018-12-25 17:03:44 +0800

    first version

---
+++ src/main/java/com/ron/wiadogs/WiadogsApplication.java
@@ -0,0 +1,14 @@
+package com.ron.wiadogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiadogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiadogsApplication.class, args);
+    }
+
+}
+
